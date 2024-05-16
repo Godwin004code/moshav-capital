93c9e158da129a129ce977fea38223b757c353ce
Godwin004code <godwinatomicsoda@gmail.com>
2024-05-16 20:12:33 +0100

    most pages done

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -11,8 +11,8 @@ const Services = () => {
     <Nav />
     <ServicesHero />
     <section className='w-[90%] mt-6 mx-auto'>
-      <section className='flex gap-16 mb-16 justify-between'>
-        <div className='bg-[#FAFFF5] w-full pl-12 py-12'>
+      <section className='flex sm:flex-col gap-16 mb-16 justify-between'>
+        <div className='bg-[#FAFFF5] w-full pl-12 sm:pl-4 sm:py-4 py-12'>
         <svg width="55" height="55" viewBox="0 0 55 55" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_318_10654)">
 <path d="M6.875 48.125H48.125" stroke="#2C3E50" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -30,11 +30,11 @@ const Services = () => {
 </clipPath>
 </defs>
 </svg>
-<h2 className='font-normal text-3xl my-6'>Financial intermediary service</h2>
+<h2 className='font-normal text-3xl my-6 sm:text-[20px]  sm:my-4'>Financial intermediary service</h2>
 <p className='text-[#000000B2]'>
 Empowering Agribusiness Growth through Financial Innovation
 </p>
-<Link to="/financial-intermidation" className='flex mt-6'><span className='my-auto mr-3 font-semibold text-xl'>Learn More</span> <svg className='my-auto' width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+<Link to="/financial-intermidation" className='flex mt-6'><span className='my-auto mr-3 font-semibold text-xl sm:text-base'>Learn More</span> <svg className='my-auto sm:w-[24px] sm:h-[24px] ' width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_318_10666)">
 <path d="M10 24H38" stroke="#1E772C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M30 32L38 24" stroke="#1E772C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -48,7 +48,7 @@ Empowering Agribusiness Growth through Financial Innovation
 </svg>
 </Link>
         </div>
-        <div className='bg-[#FAFFF5] w-full pl-16 py-12'>
+        <div className='bg-[#FAFFF5] w-full pl-12 sm:pl-4 sm:py-4 py-12'>
         <svg width="55" height="55" viewBox="0 0 55 55" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_318_10680)">
 <path d="M6.875 48.125H48.125" stroke="#2C3E50" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -64,11 +64,11 @@ Empowering Agribusiness Growth through Financial Innovation
 </defs>
 </svg>
 
-<h2 className='font-normal text-3xl my-6'>Collateral warehousing and Management</h2>
+<h2 className='font-normal text-3xl my-6 sm:text-[20px]  sm:my-4'>Collateral warehousing and Management</h2>
 <p className='text-[#000000B2]'>
 Secure Storage and Financing Solutions 
 </p>
-<Link to="/collateral-warehousing-and-collateral-management" className='flex mt-6'><span className='mr-3 my-auto font-semibold text-xl'>Learn More</span> <svg className='my-auto' width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+<Link to="/collateral-warehousing-and-collateral-management" className='flex mt-6'><span className='mr-3 my-auto font-semibold text-xl sm:text-base'>Learn More</span> <svg className='my-auto sm:w-[24px] sm:h-[24px] ' width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_318_10666)">
 <path d="M10 24H38" stroke="#1E772C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M30 32L38 24" stroke="#1E772C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -84,8 +84,8 @@ Secure Storage and Financing Solutions
         </div>
        
       </section>
-      <section className='flex gap-16 mb-10 justify-between'>
-        <div className='bg-[#FAFFF5] w-full pl-12 py-12'>
+      <section className='flex sm:flex-col gap-16 mb-10 justify-between'>
+        <div className='bg-[#FAFFF5] w-full pl-12 sm:pl-4 sm:py-4 py-12'>
         <svg width="55" height="55" viewBox="0 0 55 55" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_318_10654)">
 <path d="M6.875 48.125H48.125" stroke="#2C3E50" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -103,11 +103,11 @@ Secure Storage and Financing Solutions
 </clipPath>
 </defs>
 </svg>
-<h2 className='font-normal text-3xl my-6'>Infrastructure As-A- Service</h2>
-<p className='text-[#000000B2]'>
+<h2 className='font-normal text-3xl sm:text-[20px] my-6 sm:my-4'>Infrastructure As-A- Service</h2>
+<p className='text-[#000000B2] sm:text-sm'>
 Enhancing Efficiency and Operations in Agribusiness
 </p>
-<Link to="/infrastructure-as-a-service" className='flex mt-6'><span className='my-auto mr-3 font-semibold text-xl'>Learn More</span> <svg className='my-auto' width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+<Link to="/infrastructure-as-a-service" className='flex mt-6'><span className='my-auto mr-3 font-semibold text-xl sm:text-base'>Learn More</span> <svg className='my-auto sm:w-[24px] sm:h-[24px] '  width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_318_10666)">
 <path d="M10 24H38" stroke="#1E772C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M30 32L38 24" stroke="#1E772C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -121,7 +121,7 @@ Enhancing Efficiency and Operations in Agribusiness
 </svg>
 </Link>
         </div>
-        <div className='bg-[#FAFFF5] w-full pl-16 py-12'>
+        <div className='bg-[#FAFFF5] w-full pl-12 sm:pl-4 sm:py-4 py-12'>
         <svg width="55" height="55" viewBox="0 0 55 55" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_318_10680)">
 <path d="M6.875 48.125H48.125" stroke="#2C3E50" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -137,11 +137,11 @@ Enhancing Efficiency and Operations in Agribusiness
 </defs>
 </svg>
 
-<h2 className='font-normal text-3xl my-6'>Cluster marketing support & Brokerage sergvices</h2>
-<p className='text-[#000000B2]'>
+<h2 className='font-normal text-3xl my-6 sm:text-[20px]  sm:my-4'>Cluster marketing support & Brokerage sergvices</h2>
+<p className='text-[#000000B2] sm:text-sm'>
 Empowering Agribusiness Marketing through collaborative growth 
 </p>
-<Link to="/cluster-marketing-and-brokerage-services" className='flex mt-6'><span className='my-auto mr-3 font-semibold text-xl'>Learn More</span> <svg className='my-auto' width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+<Link to="/cluster-marketing-and-brokerage-services" className='flex mt-6'><span className='my-auto mr-3 font-semibold text-xl sm:text-base'>Learn More</span> <svg className='my-auto sm:w-[24px] sm:h-[24px] ' width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_318_10666)">
 <path d="M10 24H38" stroke="#1E772C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M30 32L38 24" stroke="#1E772C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
